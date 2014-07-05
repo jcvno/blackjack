9f6b8deef7f8743766572e1e438de7451777643c
Justin Cano <jcano001@ucr.edu>
2014-07-04 23:29:39 -0700

    print hand ranks with appropriate Ace handling

--- blackjack.py
+++ blackjack.py
@@ -2,7 +2,12 @@
 # -----------
 # Blackjack
 #
-
+# Cards are represented as strings labeled as their rank and suit
+# Examples: "7H" - 7 Hearts
+#			"TS" - 10 Spades
+#
+#
+#
 import random
 
 
@@ -18,19 +23,40 @@ def deal(deck):
 	del deck[0]
 	return card
 
+# Return the sum of the ranks in a hand
+# Face cards are of rank 10
+# Aces are of rank 11 or 1
+def rank(hand):
+	# Extract all ranks from hand
+	ranks = [10 if r == 'T' or r == 'J' or r =='Q' or r == 'K' else
+			11 if r == 'A' else
+			int(r) for r,s in hand]
+
+	# While there are 11-ranked Aces in hand and hand rank is greater than 21,
+	while 11 in ranks and sum(ranks) > 21:
+		# Change rank of Aces to 1
+		# one Ace at a time
+		# until hand rank is less than 21
+		# or until there are no more 11-ranked Aces
+		index = ranks.index(11)
+		ranks[index] = 1
+	return sum(ranks)
+
 # Print cards on screen
 # If player's turn, then hide dealer's first card
 def showCards(dealer,player,turn="player"):
-	print "Dealer Cards:"
+	print "Dealer Cards:",  rank(dealer)
 	for card in dealer[::-1]:
 		if card is dealer[0] and turn is "player":
 			card = "--"
 		print card,
 	print 
-	print "Player Cards:"
+	print "Player Cards:", rank(player)
 	for card in player:
 		print card,
 
+def blackjack():
+	return
 	
 def main():
 	deck = initDeck(1)
@@ -42,11 +68,16 @@ def main():
 	player.append(deal(deck))
 	player.append(deal(deck))
 
-	showCards.turn = "player"
-	while showCards.turn == "player":
-		showCards(dealer,player,showCards.turn)
-		
-		showCards.turn = "dealer"
+	blackjack.turn = "player"
+	while blackjack.turn == "player":
+		showCards(dealer,player,blackjack.turn)
+		choice = raw_input("\nhit or stand? ")
+
+		if choice == "hit":
+			player.append(deal(deck))
+
+		elif choice == "stand":
+			blackjack.turn = "dealer"
 
 
 if __name__ == "__main__":
