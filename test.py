2eba225c3ce021ff1bee1c853821bc0b63bbac82
Justin Cano <jcano001@ucr.edu>
2014-07-07 18:13:26 -0700

    comment formatting

--- test.py
+++ test.py
@@ -4,16 +4,16 @@ def test():
 	print
 	chips = 100
 	print "***Testing method: changeNumDecks()***"
-	"Test for valid user input"
+	"""Test for valid user input"""
 	numDecks = bj.changeNumDecks()
 	assert 0 < numDecks <= bj.MAX_DECKS
 	print "PASSED\n"
 
 	print "***Testing deck construction***"
 	deck = bj.shuffleDeck(numDecks)
-	"Test for correct number of cards in deck"
+	"""Test for correct number of cards in deck"""
 	assert len(deck) == 52*numDecks
-	"Check if all appropriate cards are in deck"
+	"""Check if all appropriate cards are in deck"""
 	for r in '23456789TJQKA'*numDecks:
 		for s in 'SHDC':
 			assert r+s in deck
@@ -22,7 +22,7 @@ def test():
 	print "PASSED\n"
 
 	print "***Testing method: deal(deck)***"
-	"Test if method correctly pops card"
+	"""Test if method correctly pops card"""
 	deck = bj.shuffleDeck(1)
 	for i in range(len(deck)):
 		bj.deal(deck)
@@ -30,42 +30,42 @@ def test():
 	print "PASSED\n"
 
 	print "***Testing method: rank(hand)***"
-	"Test validity of ranks of all cards in a deck"
+	"""Test validity of ranks of all cards in a deck"""
 	deck = bj.shuffleDeck(1)
 	for card in deck:
 		assert 0 < bj.rank([card]) <= 11
 	print "PASSED\n"
 
 	print "***Testing method: placeBet(chips)***"
-	"Test method returns a bet value that is not greater than chips value"
+	"""Test method returns a bet value that is not greater than chips value"""
 	bet = bj.placeBet(chips)
 	assert bet <= chips
 	print "PASSED\n"
 
 	print "***Testing method: menu()***"
-	"Test for valid menu() return value"
+	"""Test for valid menu() return value"""
 	choice = bj.menu()
 	assert choice in bj.menuChoices
 	print "PASSED\n"
 
 	print "***Testing method: blackjackMenu(playerCards,chips,bet)***"
-	"Test for valid menu() return value"
+	"""Test for valid menu() return value"""
 	playerCards = [bj.deal(deck), bj.deal(deck)]
 	choice = bj.blackjackMenu(playerCards,chips,bet)
 	assert choice in bj.blackjackChoices
 	print "PASSED\n"
 
 	print "***Testing method: getPayout(dealer, player, chips, bet)***"
-	"Test for player blackjack and correct payout"
+	"""Test for player blackjack and correct payout"""
 	# Blackjack pays 3:2
 	# 100 bet: 100(bet) + 150(payout) + 0(chips) = 250
 	assert bj.getPayout(['5D', '5C'], ['AH', 'JS'], 0, 100) == 250
 	# 50 bet: 50(bet) + 75(payout) + 25(chips) = 150
 	assert bj.getPayout(['5D', '5C'], ['AH', 'JS'], 25, 50) == 150
-	"Test for player bust"
+	"""Test for player bust"""
 	# 50 bet: 0(lost bet) + 0(payout) + 25(available chips) = 25
 	assert bj.getPayout(['5D', '5C'], ['3H', 'JS', 'KH'], 25, 50) == 25
-	"Test for player win"
+	"""Test for player win"""
 	# 100 bet: 100(bet) + 100(payout) + 100(chips) = 300
 	assert bj.getPayout(['TD', '7C'], ['JS', 'KH'], 100, 100) == 300
 
